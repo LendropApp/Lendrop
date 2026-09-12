96510235d397c602777c76b626bb627682ea6dea
belencitaaaaa <quijano.hidalgo2027@hilasal.superate.org>
2026-09-11 19:29:31 -0600

    locker-coverage is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -12,6 +11,7 @@ import Explore from "./pages/Explore";
 import RentalTracking from "./pages/RentalTracking";
 import OwnerDeliveryReturn from "./pages/ownerdelivery";
 import EarningsDashboard from "./pages/earningdashboard";
+import LockerCoverage from "./pages/locker-coverage";
 
 export default function App() {
   return (
@@ -25,21 +25,20 @@ export default function App() {
 <Route path="/reset-password" element={<ResetPassword />} />
 <Route path="/explore" element={<Explore />} />
 <Route path="/rental-tracking" element={<RentalTracking />} />
-
-<Route path="/owner-delivery" element={<OwnerDeliveryReturn />} />
-<Route path="/earnings-dashboard" element={<EarningsDashboard />} />
-
-<Route path="/" element={<Home />} />
-
+          <Route path="/owner-delivery" element={<OwnerDeliveryReturn />} />
+          <Route path="/earnings-dashboard" element={<EarningsDashboard />} />
+          <Route path="/locker-coverage" element={<LockerCoverage />} />
+          <Route path="/help" element={<RentalTracking />} />
           <Route
-            path="/owner-delivery"
-            element={<OwnerDeliveryReturn />}
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
           />
 
-          <Route
-            path="/earnings-dashboard"
-            element={<EarningsDashboard />}
-          />
+          <Route path="/" element={<LockerCoverage />} />
 
           {/* Protected Route */}
           <Route
@@ -54,7 +53,7 @@ export default function App() {
           {/* Fallback */}
           <Route
             path="*"
-            element={<Navigate to="/" replace />}
+            element={<Navigate to="/locker-coverage" replace />}
           />
         </Routes>
       </AuthProvider>
